0943df0d22e63bacd973d34dae892f045bbf2d7b
sam leider <sam.leider@hotmail.com>
2019-10-18 01:43:28 -0400

    pull jwt secret out of code

--- src/utils/jwtGenerate.js
+++ src/utils/jwtGenerate.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken'
+
 const jwtGenerate = id => {
-  return jwt.sign({ userId: id }, 'thisisasecret', {
+  return jwt.sign({ userId: id }, process.env.JWT_SECRET, {
     expiresIn: '7 days'
   })
 }
